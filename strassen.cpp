339699fad590f7653dab231b8db07d4108d115b9
Marwa <marwalbaadani@gmail.com>
2020-03-26 21:43:23 -0400

    file input

--- strassen.cpp
+++ strassen.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main(int argc, char **argv)
@@ -7,5 +7,37 @@ int main(int argc, char **argv)
     // declare variables
     int flag = atoi(argv[1]);
     int dimension = atoi(argv[2]);
-    int inputfile = atoi(argv[3]);
+    char *file = argv[3];
+    int n;
+    int value;
+
+    // decide when to pad with zeroes
+    if (dimension % 4 != 0)
+        n = dimension + (4 - dimension % 4);
+    else
+        n = dimension;
+
+    cout << "\n dim:" << dimension << "\nn: " << n;
+    int matrix1[n][n];
+    int matrix2[n][n];
+    ifstream in;
+    in.open(file);
+    if (!in)
+    {
+        printf("Couldn't access file\n");
+        exit(1);
+    }
+
+    cout << " opening";
+    if (in.is_open())
+    {
+        // for each number:
+        while (in >> value)
+        {
+            matrix1[];
+            cout << "\nValue: " << value; // Print out variable
+        }
+    }
+    in.close();
+    cout << "closing";
 }
